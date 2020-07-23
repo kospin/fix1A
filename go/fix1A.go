f17e062aa9d25adf8799b266e48de2a2b638db84
kospin <honeymoon0624@gmail.com>
2020-07-23 19:05:15 +0800

    go 修簡

--- go/fix1A.go
+++ go/fix1A.go
@@ -4,38 +4,21 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"strings"
 )
 
 func main() {
-	去1A尾行 := false
 	if len(os.Args) > 1 {
-
 		data, err := ioutil.ReadFile(os.Args[1])
 		if err != nil {
 			fmt.Println(err)
 		}
-
-		s := strings.Split(string(data), "\r\n")
-
-		if len(s[len(s)-1]) <= 1 {
-			s = s[:len(s)-1]
-			去1A尾行 = true
-		}
-
-		wd := ""
-		for i, t := range s {
-			wd += t
-			if i < (len(s) - 1) {
-				wd += "\r\n"
+		if data[len(data)-1] == 26 {
+			data = data[:len(data)-3]
+			werr := ioutil.WriteFile(os.Args[1], data, 0644)
+			if werr != nil {
+				fmt.Println(werr)
 			}
-		}
-		werr := ioutil.WriteFile(os.Args[1], []byte(wd), 0644)
-		if werr != nil {
-			fmt.Println(werr)
-		}
-		if 去1A尾行 {
-			fmt.Println("完成修正!")
+			println("ok")
 		}
 	}
 }
