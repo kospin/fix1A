2317547ce847b006ca0bdae734d15e93968cf060
kospin <honeymoon0624@gmail.com>
2020-07-23 18:09:21 +0800

    python 修簡

--- python/fix1A.py
+++ python/fix1A.py
@@ -1,20 +1,17 @@
 import sys
+
+
 def q(say):
     print(say)
     exit()
-if len(sys.argv)<=1:
+
+
+if len(sys.argv) <= 1:
     q("錯誤:無指定檔名")
-with open(sys.argv[1],"rt") as fin:
-    str=fin.read()
-    lines=str.split('\n')
-    if ord(lines[len(lines)-1][0])==26:
-        del lines[len(lines)-1]
-        fout =open(sys.argv[1],"wt")
-        for l in lines:
-            if l!=lines[len(lines)-1]:
-                fout.write(l+'\n')
-            else:
-                fout.write(l)
+with open(sys.argv[1], "rt") as fin:
+    str = fin.read()
+    if ord(str[-1]) == 26:
+        open(sys.argv[1], "wt").write(str[:-2])
         q("修正完成")
     else:
         q("不需更動")
