9067319cdef8e1aa686833855e1310a87b437447
kospin <honeymoon0624@gmail.com>
2020-07-23 17:27:19 +0800

    java刪餘

--- java/Fix1A.java
+++ java/Fix1A.java
@@ -1,5 +1,4 @@
 
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -9,10 +8,8 @@ import java.nio.file.Paths;
 public class Fix1A {
     public static void main(String[] args) {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
             String text = new String(Files.readAllBytes(Paths.get(args[0])));
             System.out.print(text.trim());
-            reader.close();
         } catch (final IOException e) {
             e.printStackTrace();
         }
