00ca6e80826c3c6d80b7a5306c8572a0553d0ee6
kospin <honeymoon0624@gmail.com>
2020-07-25 15:04:56 +0800

    漂亮的kotlin

--- kotlin/Fix1A.kt
+++ kotlin/Fix1A.kt
@@ -1,11 +1,8 @@
 import java.nio.file.Files
 import java.nio.file.Paths
 
-
 public fun main(args: Array<String>){
     val f =Files.readAllBytes(Paths.get(args[0]))
-    if(f.last().toInt()==26) {
-        val g = f.copyOfRange(0, f.lastIndex - 2)
-        Files.write(Paths.get(args[0]),g)
-    }
+    if(f.last().toInt()==26) 
+        Files.write(Paths.get(args[0]),f.copyOfRange(0, f.lastIndex - 2))
 }
