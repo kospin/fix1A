29c925b522beba6802bb8771457aa61e65b36eae
kospin <honeymoon0624@gmail.com>
2020-07-27 03:25:27 +0800

    java判1A

--- java/Fix1A.java
+++ java/Fix1A.java
@@ -7,7 +7,8 @@ public class Fix1A {
     public static void main(String[] args) {
         try {
             String text = new String(Files.readAllBytes(Paths.get(args[0])));
-            Files.write(Paths.get(args[0]),text.trim().getBytes());
+            if (text.getBytes()[text.getBytes().length-1]==26)
+                Files.write(Paths.get(args[0]),text.trim().getBytes());
         } catch (final IOException e) {
             e.printStackTrace();
         }
