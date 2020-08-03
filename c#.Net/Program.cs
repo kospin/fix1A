3139a1f86cbc100ccd726f85b01e96963e36ef76
kospin <honeymoon0624@gmail.com>
2020-08-03 12:03:17 +0800

    C#.Net

---
+++ c#.Net/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace myApp
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            if (args.Length >= 1)
+                try
+                {
+                    byte[] data = File.ReadAllBytes(args[0]);
+                    if (data[data.Length - 1] == 26)
+                    {
+                        byte[] fix = new byte[data.Length - 3];
+                        for (int i = 0; i < data.Length - 3; i++)
+                            fix[i] = data[i];
+                        File.WriteAllBytes(args[0], fix);
+                        Console.WriteLine("Ok");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error");
+                }
+        }
+    }
+}
